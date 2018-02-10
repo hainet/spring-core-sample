844ac973f6ab3f2075871ce1c6e7ddb90c3bdf8b
hainet <20400616+hainet@users.noreply.github.com>
2018-02-10 18:57:35 +0900

    Assertクラスのサンプル追加

--- src/test/java/com/hainet/spring/core/sample/AssertTest.java
+++ src/test/java/com/hainet/spring/core/sample/AssertTest.java
@@ -8,19 +8,70 @@ public class AssertTest {
 
     @Test(expected = IllegalStateException.class)
     public void stateTest() {
-        Assert.state(true, "State test");
-        Assert.state(false, "State test");
+        // OK
+        Assert.state(true, "Valid state.");
+
+        // NG
+        Assert.state(false, "Invalid state.");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void isTrueTest() {
-        Assert.isTrue(true, "Is true test");
-        Assert.isTrue(false, "Is true test");
+        // OK
+        Assert.isTrue(true, "Is true.");
+
+        // NG
+        Assert.isTrue(false, "Is false.");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void isNullTest() {
-        Assert.isNull(null, "Is null test");
-        Assert.isNull(new String(), "Is null test");
+        // OK
+        Assert.isNull(null, "Is null.");
+
+        // NG
+        Assert.isNull(new String(), "Is not null.");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void notNullTest() {
+        // OK
+        Assert.notNull(new String(), "Is not null.");
+
+        // NG
+        Assert.notNull(null, "Is null.");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void hasLengthTest() {
+        // OK
+        Assert.hasLength("Has length", "Text has length.");
+
+        // NG
+        Assert.hasLength("", "Text is empty.");
+        Assert.hasLength(null, "Text is null.");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void hasTextTest() {
+        // OK
+        Assert.hasText("Has length", "It has text.");
+
+        // NG
+        Assert.hasText("", "Text is empty.");
+        Assert.hasText(null, "Text is null.");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void doesNotContainTest() {
+        // OK
+        Assert.doesNotContain("Text to search", "It does not contain", "It does not contain.");
+
+        // NG
+        Assert.doesNotContain("Text to search", "Text to search", "It contains.");
+        Assert.doesNotContain("", "Does not contain", "Text to search is empty.");
+        Assert.doesNotContain(null, "Does not contain", "Text to search is null.");
+        Assert.doesNotContain("Does not contain", "", "Substring is empty.");
+        Assert.doesNotContain("Does not contain", null, "Substring is null.");
     }
 }
